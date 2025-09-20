de30e5b00427131e3d75203fd08566f271f04667
WildflowerEnam <161120109+WildflowerEnam@users.noreply.github.com>
2025-09-20 06:44:35 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
+<?php 
+include __DIR__ . "/includes/config.php";
 
+include __DIR__ .'/homepageContent/homeHeader.php'; 
+
+include __DIR__ . '/homepageContent/homeHero.php';
+include __DIR__ . '/homepageContent/problemSolutionFeatures.php';
+include __DIR__ . '/homepageContent/how-it-works.php';
+include __DIR__ . '/homepageContent/who-uses-harvesthub.php';
+include __DIR__ . '/homepageContent/testimonials.php';
+include __DIR__ . '/homepageContent/why-choose-us.php';
+include __DIR__ . '/homepageContent/call-to-action.php';
+include __DIR__ . '/homepageContent/homeFooter.php';
+?>
