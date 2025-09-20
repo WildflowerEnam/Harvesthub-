7feb4bbc0f6819bf5967df141f4cbce27bce5954
WildflowerEnam <161120109+WildflowerEnam@users.noreply.github.com>
2025-09-20 07:00:48 +0000

    Update index.php
    Added a new blocks folder to the content

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php 
 include __DIR__ . "/includes/config.php";
 
-include __DIR__ .'/homepageContent/homeHeader.php'; 
+include __DIR__ .'/blocks/homepageContent/homeHeader.php'; 
 
-include __DIR__ . '/homepageContent/homeHero.php';
-include __DIR__ . '/homepageContent/problemSolutionFeatures.php';
-include __DIR__ . '/homepageContent/how-it-works.php';
-include __DIR__ . '/homepageContent/who-uses-harvesthub.php';
-include __DIR__ . '/homepageContent/testimonials.php';
-include __DIR__ . '/homepageContent/why-choose-us.php';
-include __DIR__ . '/homepageContent/call-to-action.php';
-include __DIR__ . '/homepageContent/homeFooter.php';
+include __DIR__ . '/blocks/homepageContent/homeHero.php';
+include __DIR__ . '/blocks/homepageContent/problemSolutionFeatures.php';
+include __DIR__ . '/blocks/homepageContent/how-it-works.php';
+include __DIR__ . '/blocks/homepageContent/who-uses-harvesthub.php';
+include __DIR__ . '/blocks/homepageContent/testimonials.php';
+include __DIR__ . '/blocks/homepageContent/why-choose-us.php';
+include __DIR__ . '/blocks/homepageContent/call-to-action.php';
+include __DIR__ . '/blocks/homepageContent/homeFooter.php';
 ?>
